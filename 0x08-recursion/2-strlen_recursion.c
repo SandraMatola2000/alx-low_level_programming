fe8ca67043f139b59980a86f38c62d2fdde60ff8
sandramatola2000 <sandramatola726@gmail.com>
2023-07-05 13:14:13 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,14 +9,14 @@
 int _strlen_recursion(char *s)
 
 {
-      int longit = 0;
-        if (*s)
+	int longit = 0;
 
-        {
-                longit++;
-                longit += _strlen_recursion(s + 1);
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
 
-        }
+	}
 
-        return (longit);
+	return (longit);
 }
