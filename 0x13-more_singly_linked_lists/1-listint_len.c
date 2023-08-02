1f43bfb2f607b79bf7398f9cf9837b8c37b4bdba
sandramatola2000 <sandramatola726@gmail.com>
2023-08-02 11:25:04 -0700

    listint

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,14 @@
 size_t listint_len(const listint_t *h)
 
 {
-        size_t num = 0;
-        while (h)
+	size_t num = 0;
 
-        {
-                num++;
-     h = h->next;
-        }
-    return (num);
+	while (h)
+
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
 
 }
